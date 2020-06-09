cd99035d44476eb2e2c67bfffdf7111903cad9f8
Arushi Dwivedi <50775209+aru26@users.noreply.github.com>
2020-06-09 22:43:48 +0530

    Create Reverse integer.py

---
+++ Reverse integer.py
@@ -0,0 +1,13 @@
+class Solution:
+    # @param A : integer
+    # @return an integer
+    def reverse(self, A):
+        A = str(A)
+        if A[0] == "-":
+            A = A[0] + A[:0:-1]
+        else: A = A[::-1]
+        #check overflow
+        A = int(A)
+        if A > 2**31-1 or A < -1*(2**31 - 1):
+            return 0
+        else: return A
